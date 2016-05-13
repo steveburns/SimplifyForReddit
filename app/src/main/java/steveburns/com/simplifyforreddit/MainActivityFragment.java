e5f4e7f0fc1d7562a825cd4f8721e5d131978070
sburns <sburns@ancestry.com>
2016-05-12 20:36:29 -0600

    Place Ads on Main Fragment

--- app/src/main/java/steveburns/com/simplifyforreddit/MainActivityFragment.java
+++ app/src/main/java/steveburns/com/simplifyforreddit/MainActivityFragment.java
@@ -18,6 +18,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.squareup.picasso.Picasso;
 
 import steveburns.com.simplifyforreddit.data.SubredditsContract;
@@ -64,6 +66,19 @@ public class MainActivityFragment extends Fragment {
             // Don't have a view so we must not have any locally stored submissions... yet!
             if (view == null) {
                 view = inflater.inflate(R.layout.loading_data, container, false);
+            }
+        }
+
+        if (view != null) {
+            AdView mAdView = (AdView) view.findViewById(R.id.adView);
+            if (mAdView != null) {
+                // Create an ad request. Check logcat output for the hashed device ID to
+                // get test ads on a physical device. e.g.
+                // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+                AdRequest adRequest = new AdRequest.Builder()
+                        .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                        .build();
+                mAdView.loadAd(adRequest);
             }
         }
 
