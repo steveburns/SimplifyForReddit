ea322e61292e3da216cde21eb8b9b270d3eacaf2
sburns <sburns@ancestry.com>
2016-05-14 20:11:56 -0600

    Commit AuthConstants.java to git

---
+++ app/src/main/java/steveburns/com/simplifyforreddit/AuthConstants.java
@@ -0,0 +1,11 @@
+package steveburns.com.simplifyforreddit;
+
+/**
+ * Created by sburns on 4/28/16.
+ */
+public class AuthConstants {
+    public static final String CLIENT_ID = "-fDYP2zYaDM6cw"; //"PUT_CLIENTID_HERE";
+    public static final String APP_CREATOR = "burnssteve"; //"PUT_USERNAME_HERE";
+    public static final String CLIENT_SECRET = "4I_FHAABGE-w_ocCXeHbeAz3p6c"; //"PUT_CLIENT_SECRET_HERE";
+    public static final String PASSWORD = "asdfasdf!*"; //"PUT_PASSWORD_HERE";
+}
